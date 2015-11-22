3d712f126c1f8eec9d186fbca143db28b1af906c
RakshakTalwar <rakshak@raptorbird.com>
2015-11-21 23:55:50 -0600

    testing program

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from collections import defaultdict
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 import nltk.stem
-import pdb
+import pdb, re
 
 ### begin global instantiations
 english_stemmer = nltk.stem.SnowballStemmer('english')
@@ -21,6 +21,9 @@ class StemmedTfidfVectorizer(TfidfVectorizer):
         return lambda doc: (english_stemmer.stem(w) for w in analyzer(doc))
 
 vectorizer = StemmedTfidfVectorizer(min_df=1, stop_words='english', decode_error='ignore', strip_accents='ascii') # the vectorizer used to product the bag of words
+
+whitespace_pattern = re.compile(r'(\\\w{1}|( ){2,})') # regex pattern used to remove whitespace characters (e.g. \n) snd any whitespace consisting of more than one space (e.g. '   ')
+non_alpha_num_pattern = re.compile(r'[^0-9a-zA-Z( )]+') # regex pattern used to remove non-alphanumeric characters and spaces
 
 def get_word_bag(text_dicts):
     """Pass it a list of dictionaries. Each dictionary contains the email's text and Mongo ObjectId.Receive a bag-of-words"""
@@ -42,14 +45,18 @@ mon_col = pymongo.MongoClient('localhost', 27017)['hillary']['emails']
 # grab data from Mongo
 all_emails = [] # list will contain dicts with each dict containing the raw text and the Mongo ObjectId
 for email in mon_col.find({}, {'RawText' : 1, '_id' : 1}):
-    all_emails.append({'text' : email['RawText'], '_id' : email['_id']})
+    # clean the text
+    clean_text = re.sub(whitespace_pattern, ' ', email['RawText'])
+    clean_text = re.sub(non_alpha_num_pattern, ' ', clean_text)
+    # add the text
+    all_emails.append({'text' : clean_text, '_id' : email['_id']})
 
 # create a bag of words
 word_bag_with_ids = get_word_bag(all_emails)
 word_bag, ids = word_bag_with_ids['bag_of_words'], word_bag_with_ids['ids']
 
 # create the model
-n_clusters = 3
+n_clusters = 5
 km = KMeans(n_clusters=n_clusters, init='k-means++', n_init=1, verbose=1)
 # fit the model
 km.fit(word_bag)
@@ -59,4 +66,9 @@ clusters = defaultdict(list)
 for o,i in enumerate(km.labels_):
     clusters[i].append(ids[o])
 
-print clusters
+pdb.set_trace()
+
+# store the cluster names in the mongo documents
+for cluster in clusters:
+    for id in cluster:
+        mon_col.update({'_id' : id}, {'$set' : {'cluster' : cluster}})
